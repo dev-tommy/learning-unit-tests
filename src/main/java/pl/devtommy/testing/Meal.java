0274f800387a5d64f3b4b9616b8f24d74ff5693d
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-24 19:58:45 +0200

    Add new Meal constructor Meal(int, String)

--- src/main/java/pl/devtommy/testing/Meal.java
+++ src/main/java/pl/devtommy/testing/Meal.java
@@ -2,9 +2,15 @@ package pl.devtommy.testing;
 
 public class Meal {
     private int price;
+    private String name;
 
     public Meal(int price) {
         this.price = price;
+    }
+
+    public Meal(int price, String name) {
+        this.price = price;
+        this.name = name;
     }
 
     public int getPrice() {
