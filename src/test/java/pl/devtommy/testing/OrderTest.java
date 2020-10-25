eb9cbfd2dab4e28e3900a7275b04b1246c3bd97e
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-25 13:12:42 +0100

    Add test addingMealToOrderShouldIncreaseOrderSize()

--- src/test/java/pl/devtommy/testing/OrderTest.java
+++ src/test/java/pl/devtommy/testing/OrderTest.java
@@ -28,4 +28,18 @@ class OrderTest {
         assertThat(order.getMeals(), hasSize(0));
         assertThat(order.getMeals(), emptyCollectionOf(Meal.class));
     }
+
+    @Test
+    void addingMealToOrderShouldIncreaseOrderSize() {
+        //given
+        Meal meal = new Meal(15, "Burger");
+        Order order = new Order();
+
+        //when
+        order.addMealToOrder(meal);
+
+        //then
+        assertThat(order.getMeals(), hasSize(1));
+        assertThat(order.getMeals(), contains(meal));
+    }
 }
