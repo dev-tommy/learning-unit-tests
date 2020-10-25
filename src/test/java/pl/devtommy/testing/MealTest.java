465623a474a1884b411254cc1e550e949e032213
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-25 10:42:18 +0100

    Add matcher hamcrest - assertThat() to all test methods

--- src/test/java/pl/devtommy/testing/MealTest.java
+++ src/test/java/pl/devtommy/testing/MealTest.java
@@ -2,6 +2,8 @@ package pl.devtommy.testing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MealTest {
@@ -15,7 +17,8 @@ class MealTest {
         int discountedPrice = meal.getDiscountedPrice(7);
 
         //then
-        assertEquals(28, discountedPrice);
+        //assertEquals(28, discountedPrice);
+        assertThat(discountedPrice, is(28));
     }
 
     @Test
@@ -25,7 +28,8 @@ class MealTest {
         Meal meal2 = meal1;
 
         //then
-        assertSame(meal1, meal2);
+        //assertSame(meal1, meal2);
+        assertThat(meal1,sameInstance(meal2));
     }
 
     @Test
@@ -35,7 +39,8 @@ class MealTest {
         Meal meal2 = new Meal(10);
 
         //then
-        assertNotSame(meal1, meal2);
+        //assertNotSame(meal1, meal2);
+        assertThat(meal1,not(sameInstance(meal2)));
     }
 
     @Test
@@ -45,6 +50,7 @@ class MealTest {
         Meal meal2 = new Meal(10, "Pizza");
 
         //then
-        assertEquals(meal1, meal2);
+        //assertEquals(meal1, meal2);
+        assertThat(meal1,is(meal2));
     }
 }
