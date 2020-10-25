5cfc3838a7d367d97df6563feda2e17a959831b2
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-25 15:36:34 +0100

    Add @ParameterizedTest @EnumSource() allOrdersStatusShouldBeShorterThan15Chars()

---
+++ src/test/java/pl/devtommy/testing/OrderStatusTest.java
@@ -0,0 +1,15 @@
+package pl.devtommy.testing;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+class OrderStatusTest {
+    @ParameterizedTest
+    @EnumSource(OrderStatus.class)
+    void allOrdersStatusShouldBeShorterThan15Chars(OrderStatus orderStatus) {
+        assertThat(orderStatus.toString().length(), lessThan(15));
+    }
+}
