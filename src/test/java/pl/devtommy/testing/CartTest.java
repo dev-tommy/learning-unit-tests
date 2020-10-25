bd6ecfee4328376d5eb99723a8040e5cc6d902d2
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-25 14:59:56 +0100

    Add test cartShouldNotBeEmptyAfterAddingOrderToCart() and use assertThat( ,anyOf())

--- src/test/java/pl/devtommy/testing/CartTest.java
+++ src/test/java/pl/devtommy/testing/CartTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test cases for Cart")
@@ -20,5 +22,23 @@ class CartTest {
 
         //when, then
         assertTimeout(Duration.ofMillis(7), cart::simulateLargeOrder);
+    }
+
+    @Test
+    void cartShouldNotBeEmptyAfterAddingOrderToCart() {
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+
+        //when
+        cart.addOrderToCart(order);
+
+        //then
+        assertThat(cart.getOrders(), anyOf( //one condition is enough
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
     }
 }
