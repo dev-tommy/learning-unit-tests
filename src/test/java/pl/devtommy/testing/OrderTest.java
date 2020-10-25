7cbdcd25e9a7b6ac3dd6f97585dec65e87f26dc4
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-25 13:08:39 +0100

    Add test mealListShouldBeEmptyAfterCreationOfOrder()

--- src/test/java/pl/devtommy/testing/OrderTest.java
+++ src/test/java/pl/devtommy/testing/OrderTest.java
@@ -2,6 +2,8 @@ package pl.devtommy.testing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class OrderTest {
@@ -15,4 +17,15 @@ class OrderTest {
         assertArrayEquals(ints1, ints2);
     }
 
+    @Test
+    void mealListShouldBeEmptyAfterCreationOfOrder() {
+        // given
+        Order order = new Order();
+
+        //then
+        assertThat(order.getMeals(), empty());
+        assertThat(order.getMeals().size(), equalTo(0));
+        assertThat(order.getMeals(), hasSize(0));
+        assertThat(order.getMeals(), emptyCollectionOf(Meal.class));
+    }
 }
