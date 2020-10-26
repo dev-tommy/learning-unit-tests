cae120024cb1d88310c89aa4fddfee1beb207062
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-26 11:26:15 +0100

    Add @TestFactory calculateMealPrices() to show example of dynamic tests

--- src/test/java/pl/devtommy/testing/MealTest.java
+++ src/test/java/pl/devtommy/testing/MealTest.java
@@ -1,14 +1,17 @@
 package pl.devtommy.testing;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -119,10 +122,30 @@ class MealTest {
     }
 
     @TestFactory
-    Collection<DynamicTest> dynamicTestCollection() {
-        return Arrays.asList(
-                dynamicTest("Dynamic test 1", () -> assertThat(5, lessThan(6))),
-                dynamicTest("Dynamic test 2", () -> assertEquals(4, 2*2))
-        );
+    @DisplayName("Dynamic Test Example")
+    Collection<DynamicTest> calculateMealPrices() {
+        Order order = new Order();
+        order.addMealToOrder(new Meal(10, 2, "Hamburger"));
+        order.addMealToOrder(new Meal(7, 4, "Fries"));
+        order.addMealToOrder(new Meal(22, 3, "Pizza"));
+        Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for(int i = 0; i < order.getMeals().size(); i++) {
+            int price = order.getMeals().get(i).getPrice();
+            int quantity = order.getMeals().get(i).getQuantity();
+
+            Executable executable = () -> {
+                assertThat(calculatePrice(price, quantity) , lessThan(67));
+            };
+            String name = "Test name: " + i;
+            DynamicTest dynamicTest = DynamicTest.dynamicTest(name, executable);
+            dynamicTests.add(dynamicTest);
+        }
+
+        return dynamicTests;
+    }
+
+    private int calculatePrice(int price, int quantity) {
+        return price * quantity;
     }
 }
