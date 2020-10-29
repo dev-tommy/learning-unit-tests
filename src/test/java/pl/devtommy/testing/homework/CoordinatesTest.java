cc57a12ed032c35e798354d30e2064254c8922ba
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-10-29 11:07:23 +0100

    Refactor code style

--- src/test/java/pl/devtommy/testing/homework/CoordinatesTest.java
+++ src/test/java/pl/devtommy/testing/homework/CoordinatesTest.java
@@ -13,6 +13,7 @@ class CoordinatesTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 50, 99})
     void positionXShouldNotBeLessThan0(int x) {
+
         assertThrows(IllegalArgumentException.class, () -> new Coordinates(-1 , 0));
         assertThat(x, equalTo(new Coordinates(x, 0).getX()));
     }
@@ -20,6 +21,7 @@ class CoordinatesTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 50, 99})
     void positionYShouldNotBeLessThan0(int y) {
+
         assertThrows(IllegalArgumentException.class, () -> new Coordinates(0 , -1));
         assertThat(y, equalTo(new Coordinates(0, y).getY()));
     }
@@ -27,6 +29,7 @@ class CoordinatesTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 50, 99})
     void positionXShouldNotBeMoreThan100(int x) {
+
         assertThrows(IllegalArgumentException.class, () -> new Coordinates(101 , 0));
         assertThat(x, equalTo(new Coordinates(x, 0).getX()));
     }
@@ -34,6 +37,7 @@ class CoordinatesTest {
     @ParameterizedTest
     @ValueSource(ints = {0, 50, 99})
     void positionYShouldNotBeMoreThan100(int y) {
+
         assertThrows(IllegalArgumentException.class, () -> new Coordinates(0 , 101));
         assertThat(y, equalTo(new Coordinates(0, y).getY()));
     }
@@ -45,9 +49,11 @@ class CoordinatesTest {
             "8, 7"
     })
     void copiedCoordinatesShouldBeMoved(int x, int y) {
+
         //given
         Coordinates coordinatesToCopy = new Coordinates(x, y);
         Coordinates copiedCoordinates = Coordinates.copy(coordinatesToCopy, x, y);
+
         //when then
         assertThat(copiedCoordinates.getX(), equalTo(coordinatesToCopy.getX() + x));
         assertThat(copiedCoordinates.getY(), equalTo(coordinatesToCopy.getY() + y));
--- src/test/java/pl/devtommy/testing/homework/UnitTest.java
+++ src/test/java/pl/devtommy/testing/homework/UnitTest.java
@@ -11,88 +11,108 @@ import static org.junit.jupiter.api.Assertions.*;
 class UnitTest {
     @Test
     void unitShouldNotMoveFarThanAmountOfFuel() {
+
         //given
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, 30, 40);
-        //when than
+
+        //when then
         assertThrows(IllegalStateException.class, () -> unit.move(16,16));
     }
 
     @Test
     void fuelShouldBeUpdatedAfterMove() {
+
         //given
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, 30, 40);
+
         //when
         unit.move(5, 5);
-        //than
+
+        //then
         assertThat(unit.getFuel(), equalTo(20) );
     }
 
     @RepeatedTest(10)
     void tankUpShouldNotBeMoreThanMaxFuel() {
+
         //given
         int maxFuel = 30;
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, maxFuel, 40);
+
         //when
         unit.tankUp();
-        //than
+
+        //then
         assertThat(unit.getFuel(), lessThanOrEqualTo(maxFuel) );
     }
 
     @Test
     void cargoShouldNotWeightMoreThanUnitMaxCargoWeight() {
+
         //given
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, 30, 40);
         Cargo wood = new Cargo("wood", 41);
-        //when than
+
+        //when then
         assertThrows(IllegalStateException.class, () -> unit.loadCargo(wood));
     }
 
     @Test
     void amountOfCargoShouldIncreaseAfterLoadCargo() {
+
         //given
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, 30, 40);
         Cargo wood = new Cargo("wood", 25);
         int previousAmountOfCargo = unit.getLoad();
+
         //when
         unit.loadCargo(wood);
-        //than
+
+        //then
         assertThat(unit.getLoad(), greaterThan(previousAmountOfCargo) );
     }
 
     @Test
     void sumOfTheLoadShouldBeUpdatedAfterLoadCargo() {
+
         //given
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, 30, 40);
         int cargoWeight = 23;
         Cargo wood = new Cargo("wood", cargoWeight);
         int previousAmountOfCargo = unit.getLoad();
+
         //when
         unit.loadCargo(wood);
-        //than
+
+        //then
         assertThat(unit.getLoad(), equalTo(previousAmountOfCargo + cargoWeight) );
     }
 
     @Test
     void sumOfTheLoadShouldBeUpdatedAfterUnloadCargo() {
+
         //given
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, 30, 40);
         Cargo wood = new Cargo("wood", 23);
         unit.loadCargo(wood);
+
         //when
         unit.unloadCargo(wood);
-        //than
+
+        //then
         assertThat(unit.getLoad(), equalTo(0) );
     }
 
     @Test
     void loadShouldBeEmptyAfterUnloadAllCargo() {
+
         //given
         Coordinates startCoordinates = new Coordinates(10, 10);
         Unit unit = new Unit(startCoordinates, 30, 40);
@@ -100,9 +120,11 @@ class UnitTest {
         Cargo scrap = new Cargo("wood", 30);
         unit.loadCargo(wood);
         unit.loadCargo(scrap);
+
         //when
         unit.unloadAllCargo();
-        //than
+
+        //then
         assertThat(unit.getLoad(), equalTo(0) );
     }
 }
