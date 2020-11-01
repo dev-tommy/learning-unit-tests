9e26dbc2b078d3192059bdaef41e421c5f1e7034
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-11-01 20:31:22 +0100

    Add processCartShouldSendToPrepare() with examples of verify(), then().should() and InOrder.verify()

---
+++ src/test/java/pl/devtommy/testing/cart/CartServiceTest.java
@@ -0,0 +1,45 @@
+package pl.devtommy.testing.cart;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import pl.devtommy.testing.order.Order;
+import pl.devtommy.testing.order.OrderStatus;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+class CartServiceTest {
+
+    @Test
+    void processCartShouldSendToPrepare() {
+
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+        cart.addOrderToCart(order);
+
+        CartHandler cartHandler = mock(CartHandler.class);
+        CartService cartService = new CartService(cartHandler);
+
+        given(cartHandler.canHandleCart(cart)).willReturn(true);
+
+        //when
+        Cart resultCart = cartService.processCart(cart);
+
+        //then
+        // then(cartHandler).should().sendToPrepare(cart); //BDD methodology
+        verify(cartHandler).sendToPrepare(cart);
+        verify(cartHandler, times(1)).sendToPrepare(cart);
+        verify(cartHandler, atLeastOnce()).sendToPrepare(cart);
+
+        InOrder inOrder = inOrder(cartHandler);
+        inOrder.verify(cartHandler).canHandleCart(cart);
+        inOrder.verify(cartHandler).sendToPrepare(cart);
+
+        assertThat(resultCart.getOrders(), hasSize(1));
+        assertThat(resultCart.getOrders().get(0).getOrderStatus(), equalTo(OrderStatus.PREPARING));
+
+    }
+}
