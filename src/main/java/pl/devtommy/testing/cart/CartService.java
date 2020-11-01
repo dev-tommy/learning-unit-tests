9789d3ff3d409ed4e11533758e8865f3e9b334f2
Tomasz Frydrychowicz <tomasz.frydrychowicz.programista@gmail.com>
2020-11-01 20:10:20 +0100

    Add processCart()

---
+++ src/main/java/pl/devtommy/testing/cart/CartService.java
@@ -0,0 +1,28 @@
+package pl.devtommy.testing.cart;
+
+import pl.devtommy.testing.order.OrderStatus;
+
+public class CartService {
+
+    private CartHandler cartHandler;
+
+    public CartService(CartHandler cartHandler) {
+        this.cartHandler = cartHandler;
+    }
+
+    Cart processCart(Cart cart) {
+        if(cartHandler.canHandleCart(cart)) {
+            cartHandler.sendToPrepare(cart);
+            cart.getOrders().forEach(order -> {
+                order.changeOrderStatus(OrderStatus.PREPARING);
+            });
+            return cart;
+        } else {
+            cart.getOrders().forEach(order -> {
+                order.changeOrderStatus(OrderStatus.REJECTED);
+            });
+            return cart;
+        }
+
+    }
+}
